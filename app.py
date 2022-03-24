7c0da146929cbc353d44f439d00971463305d95f
Cesarfloz <cesarflorez280512@correo.itm.edu.co>
2022-03-24 14:09:31 -0500

    Fiirst Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+import pygeohash as gh
+
+app = Flask(__name__)
+
+
+@app.route("/saludo")
+def hello():
+    return jsonify({'response': 'hola mundo'})
+
+@app.route('/geo', methods=['GET'])
+def geo():
+    try:
+        latitud = float(request.args['lat'])
+        longitud = float(request.args['long'])
+        geohash = gh.encode(latitud, longitud, precision=12)
+    except:
+        print("Se presento un error") 
+
+    return jsonify({'latitud': latitud,
+                    'longitud': longitud,
+                    'geohash': geohash
+                    })  
+
+def suma(num1,num2):
+    return num1+num2   
+
+if __name__ == "__main__":
+    app.run()
---
+++ test_api.py
@@ -0,0 +1,10 @@
+from  app import suma
+#from  app import hello
+
+def test_suma():
+    assert suma(1,2) == 3
+
+"""
+def test_hello():
+    assert hello == ({'response': 'hola mundo'})
+"""
